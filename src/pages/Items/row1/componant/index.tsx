e96004eb94256685af3479c175126eab202763dc
Ibrahim113502 <ibrahim74485529@gmail.com>
2023-12-29 02:33:04 -0800

    food items run without errors

--- src/pages/Items/row1/componant/index.tsx
+++ src/pages/Items/row1/componant/index.tsx
@@ -1,18 +1,18 @@
 import { Flowbite } from 'flowbite-react';
 import axios, { Axios }  from 'axios';
 import React, { useState } from 'react';
-import Image1 from '../../../../assets/Images/card/biriyani.jpg'
-import Image2 from '../../../../assets/Images/card/Chiken.jpg'
-import Image3 from '../../../../assets/Images/card/burger.jpg'
-import Image4 from '../../../../assets/Images/card/Cbiriyani.jpg'
-import Image5 from '../../../../assets/Images/card/Fish.jpg'
-import Image6 from '../../../../assets/Images/card/ghee.jpg'
-import Image7 from '../../../../assets/Images/card/pasta.jpg'
-import Image8 from '../../../../assets/Images/card/pizza.jpg'
-// import Image9 from '../../../../assets/Images/card/'
+// import Image1 from 'Images/card/biriyani.jpg'
+// import Image2 from '../../../../assets/Images/card/Chiken.jpg'
+// import Image3 from '../../../../assets/Images/card/burger.jpg'
+// import Image4 from '../../../../assets/Images/card/Cbiriyani.jpg'
+// import Image5 from '../../../../assets/Images/card/Fish.jpg'
+// import Image6 from '../../../../assets/Images/card/ghee.jpg'
+// import Image7 from '../../../../assets/Images/card/pasta.jpg'
+// import Image8 from '../../../../assets/Images/card/pizza.jpg'
+// // import Image9 from '../../../../assets/Images/card/'
 import Carditems from './CardItems';
 // const [state,setstate]=useState([])
-function Itemrow1() {
+function Itemrow1(props) {
 // {Sobin anna metod not running}
 // const api=new Axios();
 //  const api = axios.create(
@@ -50,50 +50,50 @@ function Itemrow1() {
         id: 1,
         title: 'Chicken Mandi',
         desc: "Chicken Mandi is a delicious traditional dish that originates from Yemen but is widely enjoyed in various Middle Eastern countries. It's known for its aromatic rice, tender chicken, and a blend of spices",
-        icon: Image1,
+        icon: '/Image/biriyani.jpg',
     },
     
     {
         id: 2,
         title: 'Chicken Thanthurri ',
         desc: "Grilled Chicken Thanthurri presents succulent chicken marinated in spicy, tangy Thanthurri sauce, seared to perfection on the grill, offering a delightful fusion of smoky, fiery flavors.",
-        icon: Image2,
+        icon: '/Image/Chiken.jpg',
     },
     {
         id: 3,
         title: 'Chicken Burger',
         desc: "A chicken burger is a delicious sandwich consisting of a seasoned, grilled or fried chicken patty, typically served in a soft bun with assorted toppings,alternative to traditional beef burgers.",
-        icon: Image3,
+        icon: "/Image/burger.jpg"
     },
     {
         id: 4,
         title: 'Chicken Biriyani',
         desc: " Chicken Biryani is a fragrant and aromatic dish comprising flavorful basmati rice, marinated chicken, and a blend of spices, slow-cooked to create a rich, layered masterpiece of Indian cuisine.",
-        icon: Image4
+        icon: "Image/Cbiriyani.jpg"
     },
     {
         id: 5,
         title: 'Chicken Cutlet',
         desc: "Chicken cutlets are savory delights made by blending ground chicken with herbs, spices, and breadcrumbs, shaped into patties and fried to golden perfection.",
-        icon: Image5
+        icon: "Image/Fish.jpg"
     },
     {
         id: 6,
         title: 'Ghee Rice',
         desc: "Ghee rice is a fragrant and sumptuous Indian dish made by cooking long-grain Basmati rice in clarified butter, known as ghee. The rice is infused with aromatic spices",
-        icon: Image6
+        icon: "Image/ghee.jpg"
     },
     {
         id: 7,
         title: 'Pasta',
         desc: "Pasta is a versatile Italian staple made from unleavened dough, shaped into various forms , offering a comforting and customizable meal beloved worldwide.",
-        icon: Image7
+        icon: "Image/pasta.jpg"
     },
     {
         id: 8,
         title: 'Pizza',
         desc: "Pizza is an iconic Italian dish consisting of a round flatbread topped with savory tomato sauce, cheese, and various ingredients,of flavors and textures.",
-        icon: Image8
+        icon: "Image/pizza.jpg"
     }
     ];
     return (
